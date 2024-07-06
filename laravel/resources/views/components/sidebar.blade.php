60cfd0baeada338049569e68cc621144ea7abe30
RamadhanIY <ihsani.yulfa@gmail.com>
2024-07-06 15:34:21 +0700

    remake template and logout configuration

---
+++ laravel/resources/views/components/sidebar.blade.php
@@ -0,0 +1,67 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">PT Adikarya <sup>2</sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Project Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading (Ganti Font)-->
+
+
+    <div id="projects" class="sidebar-item p-3" href="#">
+        <i class="fas fa-fw fa-cog"></i>
+        <span>Projects</span>
+    </div>
+
+    <div id="logout" class="sidebar-item p-3" data-bs-toggle="modal" data-bs-target="#logoutModal">
+        <i class="fas fa-fw fa-cog"></i>
+        <span>Logout</span>
+    </div>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+    d
+    <!-- Logout Modal -->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close" onclick="closeModal('logoutModal')">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <a class="btn btn-primary" href="{{ route('login.form') }}">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    
+
+</ul>
--- laravel/resources/views/layouts/dashboard.blade.php
+++ laravel/resources/views/layouts/dashboard.blade.php
@@ -3,96 +3,72 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width,
-                   initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bootstrap5 Sidebar</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://kit.fontawesome.com/0c265f6a3f.js" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('../sb-admin-2.min.css') }}" rel="stylesheet">
+
 
 </head>
 
-<body>
-    {{-- Side Bar --}}
-    <div class="container-fluid p-0 d-flex h-100">
-        <div id="bdSidebar" 
-             class="d-flex flex-column 
-                    flex-shrink-0 
-                    p-3 bg-success
-                    text-white offcanvas-md offcanvas-start">
-            <a href="#" 
-               class="navbar-brand">
-            </a><hr>
-            <ul class="mynav nav nav-pills flex-column mb-auto">
-                <li class="nav-item mb-1">
-                    <a href="#">
-                        <i class="fa-regular fa-user"></i>
-                        Project
-                    </a>
-                </li>
+<body id="page-top">
 
-                <li class="nav-item mb-1">
-                    <a href="#">
-                        <i class="fa-regular fa-bookmark"></i>
-                        Add Project
-                    </a>
-                </li>
-                <li class="nav-item mb-1">
-                    <a href="#">
-                        <i id="logoutIcon" class="fa-solid fa-arrow-right-from-bracket"></i>
-                        Logout
-                    </a>
-                </li>
-                
-                
-            </ul>
-            <hr>
-        </div>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
 
-        <div class="bg-light flex-fill">
-            <div class="p-2 d-md-none d-flex text-white bg-success">
-                <a href="#" class="text-white" 
-                   data-bs-toggle="offcanvas"
-                   data-bs-target="#bdSidebar">
-                    <i class="fa-solid fa-bars"></i>
-                </a>
-                <span class="ms-3">GFG Portal</span>
+        @include('components.sidebar')
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                @include('components.navbar')
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                    
+
+                </div>
+                <!-- /.container-fluid -->
+
             </div>
-            
-                {{-- Page Content --}}
-                @include('components.project-list')
-            </div>
+            <!-- End of Main Content -->
 
         </div>
+        <!-- End of Content Wrapper -->
+
     </div>
+    <!-- End of Page Wrapper -->
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
     <script>
-        $(document).ready(function() {
-            $('#logoutIcon').on('click', function() {
-                $.ajax({
-                    url: '{{ route("logout") }}',
-                    type: 'POST',
-                    headers: {
-                        'X-CSRF-TOKEN': '{{ csrf_token() }}' 
-                    },
-                    success: function(response) {
-
-                        window.location.href = '{{ route("login.form") }}'; 
-                    },
-                    error: function(xhr, status, error) {
-                        console.error('Error:', error);
-                    }
-                });
-            });
-        });
+        function closeModal(modalId) {
+            var modal = document.getElementById(modalId);
+            modal.classList.remove('show');
+            modal.style.display = 'none';
+            modal.setAttribute('aria-hidden', 'true');
+        }
     </script>
-    
+
 
 </body>
 
